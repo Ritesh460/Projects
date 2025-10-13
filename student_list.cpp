a7a351f56f37cb7e1987f52857724586bb70bae3
Neptune990 <460219@bsd48.org>
2025-10-13 09:13:28 -0700

    Outline

---
+++ student_list.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+
+int main () {
+  //ask options in this function
+}
+//Create structs containing first name, last name, a number for student id, and deicmal for GPA
+//Need to have vector of struct pointer passed by reference or a vector pointer(which will point to a vector of struct pointers)
+//create ADD function for new entry of a student, add stuff in sturct and add to vector of students
+//create PRINT function for printing all students currently stored, GPA is two decimal places, and use iterators(search up)
+//create DELETE function to delet a student id and remove struct from vector
+//create QUIT function for quitting program.
