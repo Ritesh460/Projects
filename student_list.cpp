753a15a8a7696a409884d533af93fd33ed68cce6
Neptune990 <460219@bsd48.org>
2025-10-31 17:57:36 -0700

    Complete delete function and fixed SO MANY ERRORS

--- student_list.cpp
+++ student_list.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <iomanip>
 using namespace std;
 //Create structs containing first name, last name, a number for student id, and deicmal for GPA
 struct Student {
@@ -11,42 +12,46 @@ struct Student {
 };
 
 
-
 //function prototypes
-void ADD(vector<Student*> students);
-void PRINT(const vector<Student*> students);
-bool QUIT();
+void ADD(vector<Student*> &students);
+void PRINT(const vector<Student*> &students);
+void DELETE(vector<Student*> &students);
+bool QUIT(vector<Student*> &students);
 
 
 //ask options in this function(ADD,PRINT,DELETE,QUIT)
 int main () {
   char option[20];
-  vector<Student*> students;
-  cout << "Pick an option for your student list(ADD,PRINT,DELETE,QUIT): " << endl;
-  cin >> option;
-  if (option == "ADD") {
-    ADD(vector<Student*> students);
-  }
-  else if (option == "PRINT") {
-    PRINT(vector<Student*> students);
-  }
-  else if (option == "DELETE") {
-    //DELETE(vector<Student*> students)
-    cout << "deleted" << endl;
-  }
-  else if (option == "QUIT") {
-    QUIT(vector<Student*> students);
-  }
-  else {
-    cout << "Not an option. Try again!" << endl;
+  vector<Student*> students; //vector for students
+  bool running = true;
+  //while loop choosing option
+  while (running) {
     cout << "Pick an option for your student list(ADD,PRINT,DELETE,QUIT): " << endl;
     cin >> option;
+    if (strcmp(option, "ADD") == 0) {
+      ADD(students);
+    }
+    else if (strcmp(option, "PRINT") == 0) {
+      PRINT(students);
+    }
+    else if (strcmp(option, "DELETE") == 0) {
+      DELETE(students);
+    }
+    else if (strcmp(option, "QUIT") == 0) {
+      if (QUIT(students)) {
+        running = false;
+      }
+    }
+    else {
+      cout << "Not an option. Try again!" << endl;
+    }
   }
 }
 
-//Need to have vector of struct pointer passed by reference or a vector pointer(which will point to a vector of struct pointers)
+
 //create ADD function for new entry of a student, add stuff in sturct and add to vector of students
 void ADD(vector<Student*> &students) {
+   //creates new students and lists all attributes
    Student* newStudent = new Student();
    cout << "Enter first name: " << endl;
    cin >> (*newStudent).first_name;
@@ -55,30 +60,71 @@ void ADD(vector<Student*> &students) {
    cout << "Enter Student ID: " << endl;
    cin >> (*newStudent).student_id;
    cout << "Enter GPA: " << endl;
-   cin >> (*newStudent).student_id;
+   cin >> (*newStudent).GPA;
    students.push_back(newStudent);
 }
-//
+
 //create PRINT function for printing all students currently stored, GPA is two decimal places, and use iterators(search up)
 void PRINT(const vector<Student*> &students) {
+  //no students call this
+  if (students.empty()) {
+    cout << "No students." << endl;
+  }
+  //iterators for printing all students in vector
   for (const Student* s : students) {
-    cout << (*s).first_name << (*s).last_name << (*s).student_id << (*s).GPA << endl;
+    cout << (*s).first_name << ", " << (*s).last_name << ", " << (*s).student_id << ", " << fixed << setprecision(2) << (*s).GPA << endl; //using import for GPA here
   }
 }
 //create DELETE function to delete a student id and remove struct from vector
-void DELETE(vector<Student*> &students);
+void DELETE(vector<Student*> &students) {
+  //no students call this
+  if (students.empty()) {
+    cout << "No students." << endl;
+    return;
+  }
+  // initializes id for deletion and auto
   int id = 0;
   cout << "ID to delete: " << endl;
   cin >> id; 
-  cars.pop_back(id);
+  bool none = false;
+  auto i = students.begin();//searched for variable detection
+  // while loop for deletion
+  while (i != students.end()) {
+    if ((*i)->student_id == id) {
+      delete (*i);
+      i = students.erase(i);
+      cout << "Student with ID " << id << " deleted." << endl;
+      none = true;
+    }
+    else {
+     ++i;
+    }
+    
+  }
+  // no ID in there state this
+  if (none == false) {
+    cout << "No student with that ID" << endl;
+  }
+}
 //create QUIT function for quitting program.
-bool QUIT() {
+bool QUIT(vector<Student*> &students ) {
+  //char for quitting
   char positive;
   cout << "Are you sure you want to quit?(y/n)" << endl;
   cin >> positive;
-  if (positive == 'n') {
+  //quitting results in end of program
+  if (positive == 'y') {
+    for (Student* s : students) {
+     delete s;
+    }	  
     cout << "Ok thanks for playing" << endl;
-    return 0;
+    students.clear();
+    return true;
+  }
+  //otherwise return back to meny
+  else {
+    cout << "Go back to menu" << endl;
+    return false;
   }
 }
 
